01050043dc406bed67107bba75d31e596b74a4be
rbtx99 <gonias@gmail.com>
2014-06-07 16:55:09 +0100

    Round Gauges

--- js/gauge.js
+++ js/gauge.js
@@ -24,19 +24,24 @@ gauge.prototype.draw = function()
 	ctx.fillStyle = "#FF0000";
 	//ctx.fillRect(0, 0, this.canvas.width, this.canvas.width);
 	ctx.strokeStyle="#FFFF00";
-	ctx.strokeRect(0, 0, this.canvas.width, this.canvas.width);
-	ctx.moveTo(0, 0);
-	ctx.lineTo(this.canvas.width, this.canvas.height);
-	ctx.stroke();
-	ctx.moveTo(this.canvas.width, 0);
-	ctx.lineTo(0, this.canvas.height);
+	ctx.lineWidth= 4;
+	var adj= ctx.lineWidth/2-1;
+	ctx.strokeRect(adj, adj, this.canvas.width-ctx.lineWidth+1, this.canvas.height-ctx.lineWidth+1);
+	//ctx.moveTo(0, 0);
+	//ctx.lineTo(this.canvas.width, this.canvas.height);
+	//ctx.stroke();
+	//ctx.moveTo(this.canvas.width, 0);
+	//ctx.lineTo(0, this.canvas.height);
+	//ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(this.canvas.width/2, this.canvas.height/2, this.canvas.width*0.45, 0, 2*Math.PI);
 	ctx.stroke();
 	
-	ctx.font = this.canvas.height/10 + "px Arial";
+	ctx.font = this.canvas.height*0.08 + "px Arial";
 	ctx.textAlign = 'center';
 	ctx.textBaseline="middle"; 
-	ctx.fillStyle = "#00FF00";
-	ctx.fillText(this.pv, this.canvas.width/2, this.canvas.height*0.9);
+	ctx.fillStyle = "#FFFF00";
+	ctx.fillText(this.pv, this.canvas.width/2, this.canvas.height*0.85);
 	
 	var img = new Image();
 	img.src = "js/face.png";
